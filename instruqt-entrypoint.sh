cc8e4558a270b880a632d2d2df691132e3cc8b8d
Martin Goldhahn <martin.goldhahn@altibox.no>
2021-04-21 01:19:17 +0200

    add URLs for IdP

--- instruqt-entrypoint.sh
+++ instruqt-entrypoint.sh
@@ -7,4 +7,12 @@ echo "HOSTNAME: ${APIM_HOST}" >the_host_name
 sed -i "s#hostname = \"localhost\"#hostname = \"${APIM_HOST}\"#" ${WSO2_SERVER_HOME}/repository/conf/deployment.toml
 sed -i -E "s#^base_path = .+#base_path = \"https://${APIM_HOST}\"#"  ${WSO2_SERVER_HOME}/repository/conf/deployment.toml
 
+cat >> ${WSO2_SERVER_HOME}/repository/conf/deployment.toml <<EOF
+[apim.idp]
+server_url = "https://${APIM_HOST}"
+authorize_endpoint = "https://${APIM_HOST}/oauth2/authorize"
+oidc_logout_endpoint = "https://${APIM_HOST}/oidc/logout"
+oidc_check_session_endpoint = "https://${APIM_HOST}/oidc/checksession"
+EOF
+
 /home/wso2carbon/docker-entrypoint.sh
