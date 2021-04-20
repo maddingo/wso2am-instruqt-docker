25f938f2403280c78a7892bf2fce551d53858c4c
Martin Goldhahn <martin.goldhahn@altibox.no>
2021-04-20 23:46:29 +0200

    substitute base_path

--- instruqt-entrypoint.sh
+++ instruqt-entrypoint.sh
@@ -2,8 +2,9 @@
 
 APIM_HOST="wso2am-9443-${INSTRUQT_PARTICIPANT_ID}.env.play.instruqt.com"
 
-echo "HOSTNAME: ${APIM_HOST}"
+echo "HOSTNAME: ${APIM_HOST}" >the_host_name
 
-sed -i 's#hostname = "localhost"#hostname = "${APIM_HOST}"#' ${WSO2_SERVER_HOME}/repository/conf/deployment.toml
+sed -i "s#hostname = \"localhost\"#hostname = \"${APIM_HOST}\"#" ${WSO2_SERVER_HOME}/repository/conf/deployment.toml
+sed -i -E "s#^base_path = .+#base_path = https://${APIM_HOST}#"  ${WSO2_SERVER_HOME}/repository/conf/deployment.toml
 
 /home/wso2carbon/docker-entrypoint.sh
